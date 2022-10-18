f3b18e8582faffb62e92e2581dfc2c7ba36518f1
AlexeyZh <waltafunk@gmail.com>
2022-10-18 23:21:43 +0300

    refactor

--- src/Game.kt
+++ src/Game.kt
@@ -3,30 +3,28 @@ import java.lang.Math.*
 
 fun main(args: Array<String>) {
     val options = arrayOf("Rock", "Paper", "Scissors")
-    val gameChoise = getGameChoise(options)
+    val gameChoice = getGameChoice(options)
     val userChoice = getUserChoice(options)
 }
 
-fun getGameChoise(options: Array<String>) {
-    return optionsParam[(Math.random() * optionsParam.size).toInt()]
-}
+fun getGameChoice(optionsParam: Array<String>) =
+    optionsParam[(random() * optionsParam.size).toInt()]
 
-fun getUserChoice(options: Array<String>): String {
+fun getUserChoice(optionsParam: Array<String>): String {
     var isValidChoice = false
     var userChoice = ""
-    while (!isValidChoice) {
-        //Запросить у пользователя его выбор
+
+    while (!isValidChoice){
         print("Please enter one of the following:")
         for (item in optionsParam) print(" $item")
         println(".")
-        //Прочитать пользовательский ввод
+
         val userInput = readLine()
-        //Проверить пользовательский ввод
         if (userInput != null && userInput in optionsParam) {
             isValidChoice = true
             userChoice = userInput
         }
-        //Если выбран недопустимый вариант, сообщить пользователю
+
         if (!isValidChoice) println("You must enter a valid choice.")
     }
     return userChoice
